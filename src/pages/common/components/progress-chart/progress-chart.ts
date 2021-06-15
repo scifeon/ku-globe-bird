7ff99e76bdcccd0e8c8d9dc23e7ec8983c207364
Martin Asser Hansen <asser@scifeon.com>
2021-06-15 11:39:38 +0200

    TI2: ApexChart rendering

--- src/pages/common/components/progress-chart/progress-chart.ts
+++ src/pages/common/components/progress-chart/progress-chart.ts
@@ -1,6 +1,53 @@
-import { customElement, autoinject } from "aurelia-framework";
+import { IApexChartConfig } from "@scifeon/ui";
+import ApexCharts from "apexcharts/dist/apexcharts.common.js";
+import { bindable, customElement } from "aurelia-framework";
 
 @customElement("progress-chart")
 export class ProgressChart {
+    @bindable public readonly data: any;
 
+    public chartConfig: IApexChartConfig = {
+        chart: {
+            type: 'bar',
+            height: 350,
+            events: {
+                dataPointSelection: (event, chartContext, config) => {
+                    console.log("LIGE HER", chartContext, config, event);
+                }
+            },
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        series: [
+            {
+                data: [
+                    {
+                        x: 'Apple',
+                        y: 54
+                    },
+                    {
+                        x: 'Orange',
+                        y: 66
+                    },
+                ],
+            },
+        ],
+        xaxis: {
+          type: 'category'
+        }
+    };
+
+    public chart: ApexCharts;
+
+    bind() {
+        const data =  this.chartConfig.series[0]["data"];
+
+        data.splice(0, data.length, ...this.data);
+    }
 }
--- src/pages/stats/stats.ts
+++ src/pages/stats/stats.ts
@@ -8,4 +8,23 @@ import { autoinject } from "aurelia-framework";
 @autoinject
 @scifeonRoute({ title: "Birds", route: "b10k/stats" })
 export class BirdsPage {
+
+    public data = [
+                    {
+                        x: 'Fisk',
+                        y: 44
+                    },
+                    {
+                        x: 'Fugl',
+                        y: 36
+                    },
+                    {
+                        x: 'Hammer',
+                        y: 16
+                    },
+                    {
+                        x: 'Ost',
+                        y: 96
+                    },
+                ];
 }
