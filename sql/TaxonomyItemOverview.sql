bd09685c1f8285be81fefde7cda80a0cc7e522cd
Martin Asser Hansen <asser@scifeon.com>
2021-06-22 20:42:13 +0200

    TI2: more view testing

--- sql/TaxonomyItemOverview.sql
+++ sql/TaxonomyItemOverview.sql
@@ -1,70 +1,32 @@
-SELECT DISTINCT
-	t.id,
-	t.name AS LatinName,
-	t.FamilyName,
-	t.GenusName,
-	t.SpeciesName,
-	json_value(t.attributes, '$.speciesEnglishName') AS CommonName,
-	si.id AS SampleID,
-	a.id AS AnimalID,
-	e.id AS ExperimentID,
-	e.name AS ExperimentName,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'DNA extraction') AS ExtractionStatus,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'DNA quality testing') AS QualityTestingStatus,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'Library preparation') AS LibraryPreparationStatus,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'Sequencing') AS SequencingStatus,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'Genome assembly') AS GenomeAssemblyStatus,
-	(
-	SELECT
-		status
-	FROM
-		WFM_Step
-	WHERE
-		ExperimentID = e.id
-		AND name = 'Genome qualification') AS GenomeQualificationStatus
+SELECT
+	tax.Name AS LatinName,
+	tax.FamilyName,
+	tax.GenusName,
+	tax.SpeciesName,
+	COUNT(smp.ID) AS SampleCount,
+	JSON_VALUE(tax.Attributes,
+	'$.speciesEnglishName') AS SpeciesEnglishName,
+	lsmp.Name AS LatestSampleName,
+	JSON_VALUE(lsmp.Attributes,
+	'$.sampleDataLevel') AS LatestSampleDataLevel
 FROM
-	Bio_TaxonomyItem t
-LEFT JOIN PnS_Animal a ON
-	a.TaxonomyItemID = t.id
-LEFT JOIN WFM_V_StepSampleInput si ON
-	si.AnimalID = a.ID
-	AND InputPosition = 0
-LEFT JOIN WFM_Step st ON
-	st.ID = si.StepID
-LEFT JOIN WFM_Experiment e ON
-	e.ID = st.ExperimentID;
+	Bio_TaxonomyItem AS tax CROSS APPLY (
+	SELECT
+		TOP 1 latestSmp.Name,
+		latestSmp.Attributes
+	FROM
+		PnS_Sample AS latestSmp
+	WHERE
+		latestSmp.TaxonomyItemID = tax.ID
+	ORDER BY
+		CreatedUtc DESC ) AS lsmp
+LEFT JOIN PnS_Sample AS smp ON
+	smp.TaxonomyItemID = tax.ID
+GROUP BY
+	tax.name,
+	tax.FamilyName,
+	tax.GenusName,
+	tax.SpeciesName,
+	tax.Attributes,
+	lsmp.Name,
+	lsmp.Attributes
