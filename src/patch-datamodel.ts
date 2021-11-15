1d54abaa333801292ba6faf5b1da7d21feb80d86
Martin Asser Hansen <asser@scifeon.com>
2021-11-15 14:37:37 +0100

    B10K: adding a patch_datamodel.ts file

---
+++ src/patch-datamodel.ts
@@ -0,0 +1,30 @@
+import { DataModel } from "@scifeon/core";
+import { PLUGIN_TYPE, scifeonPlugin } from "@scifeon/plugins";
+
+@scifeonPlugin({
+    type: PLUGIN_TYPE.BOOTSTRAPPER,
+})
+export class B10KPatchDatamodel {
+    constructor(
+        private datamodel: DataModel,
+    ) { }
+
+    /**
+     * Setting Sample name and attributes.status labels.
+     */
+    private patchSample() {
+        const sampleTable = this.datamodel.findTable("Sample");
+
+        const nameField = sampleTable.fields.find(f => f.accessor === "name");
+
+        nameField.label = "B10K ID";
+
+        const statusField = sampleTable.fields.find(f => f.accessor === "attributes.status");
+
+        statusField.label = "Status";
+    }
+
+    public async init() {
+        this.patchSample();
+    }
+}
