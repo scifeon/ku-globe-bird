ec0816ec014a0b6115382afe82909ca52f68d9e6
Martin Asser Hansen <asser@scifeon.com>
2022-02-10 14:20:26 +0100

    Removing stats page

--- src/public_pages/stats/components/phylogeny-table/phylogeny-table.ts
+++
@@ -1,9 +0,0 @@
-import { customElement } from "aurelia-framework";
-import { IListViewConfig } from "@scifeon/ui";
-import { PHYLOGENY_LIST_CONFIG } from "./static/phylogeny-list-config.static";
-
-@customElement("phylogeny-table")
-export class PhylogenyTable {
-    public records: any[] = [];
-    public listViewConfig: IListViewConfig = PHYLOGENY_LIST_CONFIG;
-}
--- src/public_pages/stats/components/phylogeny-table/static/phylogeny-list-config.static.ts
+++
@@ -1,8 +0,0 @@
-import { IListViewConfig } from "@scifeon/ui";
-
-export const PHYLOGENY_LIST_CONFIG: IListViewConfig = {
-    fields: [
-        { accessor: "family" },
-        { accessor: "genus" },
-    ],
-}
